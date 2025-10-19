96ca09f967ac1353facf41a0bf7c9d9ee87ac513
straberiwine74 <straberiwine@gmail.com>
2025-10-19 14:10:30 -0500

    update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import requests
+import requests, os
 
 app = Flask(__name__)
 
@@ -11,3 +11,7 @@ def hello():
 def get_tree():
     person = request.args.get("person", "John Doe")
     return {"requested_person": person, "records": []}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
